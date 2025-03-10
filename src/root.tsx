b104e39e0c1a542005e7915651c6ad192525e255
Tu Nguyen <tunnm@zigvy.com>
2025-03-10 15:59:28 +0700

    feat: update single-spa configs

---
+++ src/root.tsx
@@ -0,0 +1,13 @@
+import { StrictMode } from 'react';
+import './index.css';
+import App from './App';
+
+const Root = () => {
+  return (
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
+};
+
+export default Root;
--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,23 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: 'http://localhost:3000',
   plugins: [react()],
+  server: {
+    port: 3000,
+    cors: true,
+  },
+  build: {
+    // target: 'esnext',
+    // cssCodeSplit: false,
+    rollupOptions: {
+      preserveEntrySignatures: 'strict',
+      input: 'src/hr-forte-webapp.tsx',
+      output: {
+        format: 'esm',
+        entryFileNames: 'hr-forte-webapp.js',
+      },
+      external: ['single-spa'],
+    },
+  },
 });
